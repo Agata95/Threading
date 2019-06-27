9a4ee578d3211409e816338991b20f7df6bf37ea
Agata <agata.weltrowska@outlook.com>
2019-06-27 20:57:15 +0200

    synchronized lock

--- src/main/java/com/javagda25/Threading/banking_race/KontoBankowe.java
+++ src/main/java/com/javagda25/Threading/banking_race/KontoBankowe.java
@@ -2,16 +2,23 @@ package com.javagda25.Threading.banking_race;
 
 public class KontoBankowe {
     private double stanKonta;
+    // synchronizowanie metod, aby wątki poczekały w kolejce na swoją kolej
+    // aby nie nachodziły na siebie w momencie dodawania/odejmowania pieniędzy z konta
+    private final Object lock = new Object();
 
     public double getStanKonta() {
         return stanKonta;
     }
 
-    public void przelewPrzychodzacy(double kwota){
-        stanKonta+=kwota;
+    public void przelewPrzychodzacy(double kwota) {
+        synchronized (lock) {
+            stanKonta += kwota;
+        }
     }
 
-    public void przelewWychodzacy(double kwota){
-        stanKonta-=kwota;
+    public void przelewWychodzacy(double kwota) {
+        synchronized (lock) {
+            stanKonta -= kwota;
+        }
     }
 }
